5ec27be954c8e0c896245ba2e7a3a1756f90c053
l3miage-papeauw <119318109+l3miage-papeauw@users.noreply.github.com>
2023-03-05 22:05:29 +0100

    BooksTDO

--- app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
+++ app/src/main/java/fr/uga/l3miage/library/books/BookDTO.java
@@ -1,11 +1,13 @@
 package fr.uga.l3miage.library.books;
 
 import fr.uga.l3miage.library.authors.AuthorDTO;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Collection;
 
 public record BookDTO(
         Long id,
+        @NotBlank
         String title,
         long isbn,
         String publisher,
