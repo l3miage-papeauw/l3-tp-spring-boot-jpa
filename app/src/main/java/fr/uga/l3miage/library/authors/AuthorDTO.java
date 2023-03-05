4c6e2142b35f846f29d8b4a28d22798b5039d23a
l3miage-papeauw <119318109+l3miage-papeauw@users.noreply.github.com>
2023-03-05 22:01:48 +0100

    AuthorDTO

--- app/src/main/java/fr/uga/l3miage/library/authors/AuthorDTO.java
+++ app/src/main/java/fr/uga/l3miage/library/authors/AuthorDTO.java
@@ -1,7 +1,11 @@
 package fr.uga.l3miage.library.authors;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthorDTO(
+
         Long id,
+        @NotBlank
         String fullName
 ) {
 }
